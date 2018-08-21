c8eeefc1912f6bb9a17b4aaad4d5df9e6b638afc
michaelSG42 <_>
2018-08-21 23:15:30 +0200

    Read buttons based on millis(), integrate displayTest() by three middle clicks, rename variables without _ and delete old trash.

--- NumeralSystemsClock.ino
+++ NumeralSystemsClock.ino
@@ -12,13 +12,13 @@ uint8_t lastSecond;
 uint8_t setSecond;
 uint8_t setMinute;
 uint8_t setHour;
-uint8_t stopTime;
 uint8_t stopCenti;
 uint8_t stopDeci;
-/* Numeral base to start with centiseconds: */
-const uint8_t STOP_BASE_CENTI = 5;
+uint8_t stoppedTime;
+const uint8_t STOP_BASE_CENTI = 5; /* Numeral base to start with centiseconds: */
 unsigned long lastEventMillis;
 unsigned long startStopClock;
+bool stopClockRunning;
 bool isChanged;
 
 /* Initialize LedControl/MAX7219. */
@@ -35,28 +35,30 @@ uint8_t brightness = 1;
 
 /* Buttons INPUT_PULLUP */
 const uint8_t BUTTON[3] = {5, 6, 7};
-long button_pressed[3];
-long button_status[3];
-float last_pressed_time[3];
-float pre_last_pressed_time[3];
-bool button_evaluated[3] = {false, false, false};
+bool buttonLastStatus[3];
+bool buttonIsPressed[3];
+bool buttonIsHold[3];
+bool buttonEvaluated[3];
+uint8_t buttonClicks[3];
+uint8_t buttonIsClicked[3];
+unsigned long buttonMillis[3];
 
 /* Numeral systems */
 uint8_t base = 2;
-uint8_t previous_base;
+uint8_t preBase;
 const char REPRESENTATION[16] = { '0', '1', '2', '3', '4', '5', '6', '7',
                                   '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 const uint8_t SHOW_BASE_SECS = 4;
-int show_base = SHOW_BASE_SECS;
+int showBase = SHOW_BASE_SECS;
 
 /* Clock modes:
  * 0: clock
  * 1: set time
  * 2: stopwatch
  */
-uint8_t clock_mode = 0;
-uint8_t previous_clock_mode;
-uint8_t current_row;
+uint8_t clockMode = 0;
+uint8_t preClockMode;
+uint8_t currentRow;
 
 void setup() {
 
@@ -73,7 +75,6 @@ void setup() {
   }
 
   /* Setup clock. */
-  //Wire.begin();
   readRTC();
 
   /* Setup buttons. */
@@ -81,233 +82,255 @@ void setup() {
     pinMode(BUTTON[i], INPUT_PULLUP);
   }
 
-  //display_test(50);
-
 }
 
 void loop() {
 
-  button_commands();
+  readButtons();
+  buttonActions();
 
   if (second() != lastSecond) {
 
-    if (show_base > -1) {
-      show_base--;
-    }
-
-    if (clock_mode == 0) {
+    if (clockMode == 0) {
+      if (showBase > -1) {
+        showBase--;
+      }
       lastSecond = second();
-      print_clock(second(), minute(), hour());
+      printClock(second(), minute(), hour());
     }
 
-    if (clock_mode == 1) {
+    if (clockMode == 1) {
       lastSecond = second();
       lastEventMillis = millis();
-      print_clock(setSecond, setMinute, setHour);
+      printClock(setSecond, setMinute, setHour);
     }
 
   }
 
-  if (clock_mode == 1 && (millis() - lastEventMillis) >= 500) {
+  if (clockMode == 1 && (millis() - lastEventMillis) >= 500) {
+
     lastEventMillis = millis();
+
     /* If BUTTON[0] and BUTTON[2] not pressed: */
-    if (button_pressed[0] == button_pressed[2]) {
-      lc.clearDisplay(current_row);
+    if (buttonIsPressed[0] == buttonIsPressed[2]) {
+      lc.clearDisplay(currentRow);
     }
+
   }
 
-  if (clock_mode == 2) {
-
-    stopTime = now() - startStopClock;
-
-    if (lastSecond != second(stopTime)) {
-      lastSecond = second(stopTime);
-      stopCenti = 0;
-      stopDeci = 0;
-      isChanged = true;
-    }
+  if (clockMode == 2 && stopClockRunning) {
 
     if (millis() - lastEventMillis >= 10) {
-      while (millis() - lastEventMillis >= 10) {
-        lastEventMillis += 10;
-        if (stopCenti < 99) {
-          stopCenti++;
-        }
-      }
-      if (stopDeci != stopCenti / 10) {
-        stopDeci = stopCenti / 10;
-        isChanged = true;
-      }
-      if (base >= STOP_BASE_CENTI) {
-        print_clock(stopCenti, second(stopTime), minute(stopTime));
-      }
-      else if (isChanged) {
-        isChanged = false;
-        print_clock(stopDeci, second(stopTime), minute(stopTime));
-      }
+      giveStopClock(now() - startStopClock, true);
     }
 
   }
 
 }
 
-void button_commands() {
+void readButtons() {
 
   for (int i = 0; i < 3; i++) {
+
     if (digitalRead(BUTTON[i]) == LOW) {
-      button_pressed[i]++;
+
+      if (!buttonLastStatus[i]) {
+        buttonLastStatus[i] = true;
+        buttonMillis[i] = millis();
+      }
+
+      if (millis() - buttonMillis[i] >= 20) {
+        buttonIsPressed[i] = true;
+      }
+
+      if (millis() - buttonMillis[i] >= 1000) {
+        buttonIsHold[i] = true;
+      }
+
     }
+
     else {
-      button_status[i] = button_pressed[i];
-      button_pressed[i] = 0;
-      button_evaluated[i] = false;
-      if (button_status[i] > 50) {
-        pre_last_pressed_time[i] = last_pressed_time[i];
-        last_pressed_time[i] = micros();
-      }
-    }
-  }
 
-  if (button_pressed[2] > 50) {
-    if (clock_mode == 0 || clock_mode == 2) {
-      if (button_pressed[1] > 50) {
-        /* Do not interpret as long klick (time setting). */
-        button_evaluated[1] = true;
-        /* Do not interpret as short klick (show numeral base). */
-        button_pressed[1] = 30000;
-        if (brightness < 15) {
-          brightness++;
-          set_brightness();
-          button_pressed[2] = -5000;
-        }
-      }
-      else {
-        if (base < 16) {
-          base++;
-          show_base = SHOW_BASE_SECS;
-          print_clock(second(), minute(), hour());
-          button_pressed[2] = -11000;
-        }
-      }
-    }
-    if (clock_mode == 1) {
-      if (current_row == 2 && setHour < 24) {
-        setHour++;
-        print_clock(setSecond, setMinute, setHour);
-      }
-      if (current_row == 1 && setMinute < 60) {
-        setMinute++;
-        print_clock(setSecond, setMinute, setHour);
-      }
-      if (current_row == 0 && setSecond < 60) {
-        setSecond++;
-        print_clock(setSecond, setMinute, setHour);
-      }
-      button_pressed[2] = -5000;
-    }
-  }
+      buttonIsHold[i] = false;
 
-  if (button_pressed[0] > 50) {
-    if (clock_mode == 0 || clock_mode == 2) {
-      if (button_pressed[1] > 50) {
-        /* Do not interpret as long klick (time setting). */
-        button_evaluated[1] = true;
-        /* Do not interpret as short klick (show numeral base). */
-        button_pressed[1] = 30000;
-        if (brightness > 0) {
-          brightness--;
-          set_brightness();
-          button_pressed[0] = -5000;
-        }
+      if (buttonLastStatus[i]) {
+        buttonMillis[i] = millis();
+        buttonLastStatus[i] = false;
       }
-      else {
-        if (base > 2) {
-          base--;
-          show_base = SHOW_BASE_SECS;
-          print_clock(second(), minute(), hour());
-          button_pressed[0] = -11000;
-        }
-      }
-    }
 
-    if (clock_mode == 1) {
-      if (current_row == 2 && setHour > 0) {
-        setHour--;
-        print_clock(setSecond, setMinute, setHour);
-      }
-      if (current_row == 1 && setMinute > 0) {
-        setMinute--;
-        print_clock(setSecond, setMinute, setHour);
-      }
-      if (current_row == 0 && setSecond > 0) {
-        setSecond--;
-        print_clock(setSecond, setMinute, setHour);
-      }
-      button_pressed[0] = -5000;
-    }
-  }
-
-  if (button_pressed[1] > 30000) {
-    if (!button_evaluated[1]) {
-      if (clock_mode == 0) {
-        clock_mode = 1;
-        current_row = NUM_DRIVERS - 1;
-        setSecond = second();
-        setMinute = minute();
-        setHour = hour();
-      }
-      else {
-        clock_mode = 0;
-        writeRTC();
-        readRTC();
-      }
-      print_clock(setSecond, setMinute, setHour);
-      button_evaluated[1] = true;
-    }
-  }
-
-  if (button_status[1] > 50 && button_status[1] < 30000) {
-    if (last_pressed_time[1] - pre_last_pressed_time[1] < 500000.0) {
-      if (clock_mode == 0) {
-        startStopClock = now();
-        stopDeci = 0;
-        stopCenti = 0;
-        clock_mode = 2;
-      }
-      else if (clock_mode == 2) {
-        clock_mode = 0;
-      }
-    }
-    else {
-      if (clock_mode == 0) {
-        show_base = SHOW_BASE_SECS;
-        print_clock(second(), minute(), hour());
-      }
-      if (clock_mode == 1) {
-        if (current_row == 0) {
-          current_row = NUM_DRIVERS - 1;
+      if (buttonIsPressed[i] && (millis() - buttonMillis[i] >= 50)) {
+        /* Double clicks only for middle button: */
+        if (i == 1) {
+          buttonClicks[i]++;
+          buttonIsPressed[i] = false;
         }
         else {
-          current_row--;
+          buttonIsClicked[i] = 1;
+          buttonIsPressed[i] = false;
         }
       }
+
+      if (buttonClicks[i] > 0 && (millis() - buttonMillis[i] >= 100)) {
+        buttonIsClicked[i] = buttonClicks[i];
+        buttonClicks[i] = 0;
+      }
+
     }
+
   }
 
 }
 
-void display_test(int blink_delay) {
+void buttonActions() {
+
+  /* Left and right button actions: */
+
+  for (int i = 0; i < 3; i = i + 2) {
+
+    if (buttonIsHold[i]) {
+      buttonIsHold[i] = false;
+      buttonMillis[i] = millis() - 750;
+      buttonIsClicked[i] = 1;
+    }
+
+    if (buttonIsClicked[i] == 1) {
+
+      buttonIsClicked[i] = 0;
+
+      if (buttonIsPressed[1]) {
+        buttonEvaluated[1] = true;
+        changeBright(i);
+        setBright();
+      }
+
+      else {
+
+        if (clockMode == 1) {
+          changeSetTime(i);
+          printClock(setSecond, setMinute, setHour);
+        }
+        else {
+          changeBase(i);
+          showBase = SHOW_BASE_SECS;
+          if (clockMode == 0 || clockMode == 1) {
+            printClock(second(), minute(), hour());
+          }
+          if (clockMode == 2 && !stopClockRunning) {
+            giveStopClock(stoppedTime, false);
+          }
+        }
+
+      }
+
+    }
+
+  }
+
+  /* If middle button was used for combination with left or right button, do not interpret as click, too. */
+
+  if (buttonEvaluated[1] && buttonIsClicked[1] == 1) {
+    buttonEvaluated[1] = false;
+    buttonIsClicked[1] = 0;
+  }
+
+  /* Middle button actions: */
+
+  if (buttonIsClicked[1] == 1) {
+
+    buttonIsClicked[1] = 0;
+
+    if (clockMode == 0) {
+      showBase = SHOW_BASE_SECS;
+      printClock(second(), minute(), hour());
+    }
+
+    if (clockMode == 1) {
+      if (currentRow == 0) {
+        currentRow = NUM_DRIVERS - 1;
+      }
+      else {
+        currentRow--;
+      }
+    }
+
+    if (clockMode == 2) {
+      if (stopClockRunning) {
+        stoppedTime = now() - startStopClock;
+        giveStopClock(stoppedTime, true);
+      }
+      else {
+        lastEventMillis = millis();
+        startStopClock = now() - stoppedTime;
+        giveStopClock(stoppedTime, true);
+      }
+      stopClockRunning = !stopClockRunning;
+    }
+
+  }
+
+  /* Multiple clicks only from middle button: */
+
+  if (buttonIsClicked[1] == 2) {
+
+    buttonIsClicked[1] = 0;
+
+    if (clockMode == 0) {
+      stopClockRunning = false;
+      stoppedTime = 0;
+      stopDeci = 0;
+      stopCenti = 0;
+      clockMode = 2;
+      showBase = 1;
+      printClock(0, 0, 0);
+    }
+    else if (clockMode == 2) {
+      clockMode = 0;
+    }
+
+  }
+
+  if (buttonIsClicked[1] > 2) {
+    buttonIsClicked[1] = 0;
+    displayTest(40);
+  }
+
+  /* Button holds for middle button: */
+
+  if (buttonIsHold[1] && !buttonEvaluated[1] && !buttonIsPressed[0] && !buttonIsPressed[2]) {
+
+    buttonEvaluated[1] = true;
+
+    if (clockMode == 0) {
+      clockMode = 1;
+      currentRow = NUM_DRIVERS - 1;
+      setSecond = second();
+      setMinute = minute();
+      setHour = hour();
+    }
+    else if (clockMode == 1) {
+      clockMode = 0;
+      writeRTC();
+      readRTC();
+    }
+
+    printClock(setSecond, setMinute, setHour);
+
+  }
+
+}
+
+void displayTest(int blinkDelay) {
 
   bool m = true;
 
-  clear_displays();
-  
+  clearDisplays();
+
   for (int i = 0; i < NUM_DRIVERS; i++) {
     for (int j = 0; j < NUM_DIGITS[i]; j++) {
       for (int k = 0; k < 2; k++) {
         for (int l = 0; l < 8; l++) {
           lc.setLed(i, j, l, m);
-          delay(blink_delay);
+          delay(blinkDelay);
         }
         m = !m;          
       }
@@ -319,16 +342,16 @@ void display_test(int blink_delay) {
       for (int i = 0; i < NUM_DRIVERS; i++) {
         for (int j = 0; j < NUM_DIGITS[i]; j++) {
           lc.setLed(i, j, l, m);
-          delay(blink_delay);
+          delay(blinkDelay);
         }  
       }
     m = !m;
     }
   }
-  
+
 }
 
-void binary(int number, int row, int first_digit) {
+void binary(int number, int row, int firstDigit) {
 
   /* (number & 1<<i) gives 0 or number,
    * ! negates => 1 or 0), but need 1 for true => !!(number & i<<i).
@@ -343,89 +366,82 @@ void binary(int number, int row, int first_digit) {
    *    }
    *  } */
 
-  for (int i = 0; i < NUM_DIGITS[row] - first_digit; i++) {
-    lc.setDigit(row, first_digit + i, !!(number & 1<<i), false);
+  for (int i = 0; i < NUM_DIGITS[row] - firstDigit; i++) {
+    lc.setDigit(row, firstDigit + i, !!(number & 1<<i), false);
   }
-  
+
 }
 
-void print_clock(uint8_t bottom, uint8_t middle, uint8_t top) {
+void printClock(uint8_t bottom, uint8_t middle, uint8_t top) {
 
   int digits;
 
-  if (base != previous_base) {
-    clear_displays();
-    previous_base = base;
+  if (base != preBase) {
+    clearDisplays();
+    preBase = base;
   }
 
-  if (clock_mode != previous_clock_mode) {
-    clear_displays();
-    previous_clock_mode = clock_mode;
+  if (clockMode != preClockMode) {
+    clearDisplays();
+    preClockMode = clockMode;
   }
 
   if (base > 2) {
-    if (show_base > 0) {
+    if (showBase > 0) {
       if (base < 10) {
         digits = 1;
       }
       else {
         digits = 2;
       }
-      print_digits(base, 0, NUM_DIGITS[0] - digits, 16, 10);
+      printDigits(base, 0, NUM_DIGITS[0] - digits, 16, 10);
     }
-    else if (show_base == 0) {
+    else if (showBase == 0) {
       lc.clearDisplay(0);
     }
   }
 
-  if (clock_mode == 0 || clock_mode == 1) {
-    print_digits(bottom, 0, 0, 59, base);
-    print_digits(middle, 1, 0, 59, base);
-    print_digits(top, 2, 0, 23, base);
+  if (clockMode == 0 || clockMode == 1) {
+    printDigits(bottom, 0, 0, 59, base);
+    printDigits(middle, 1, 0, 59, base);
+    printDigits(top, 2, 0, 23, base);
   }
-  if (clock_mode == 2) {
+  if (clockMode == 2) {
     if (base >= STOP_BASE_CENTI) {
-      print_digits(bottom, 0, 0, 99, base);
+      printDigits(bottom, 0, 0, 99, base);
     }
     else {
-      print_digits(bottom, 0, 0, 9, base);
+      printDigits(bottom, 0, 0, 9, base);
     }
-    print_digits(middle, 1, 0, 59, base);
-    print_digits(top, 2, 0, 59, base);
+    printDigits(middle, 1, 0, 59, base);
+    printDigits(top, 2, 0, 59, base);
   }
-
-  /*
-  binary(seconds, 0, 0);
-  binary(minutes, 1, 0);
-  binary(hours, 2, 0);
-  */
 
 }
 
-void print_digits(int number, int row, int first_digit, int maximum, int numeral_base) {
+void printDigits(int number, int row, int firstDigit, int maximum, int numeralBase) {
 
-  unsigned long power[NUM_DIGITS[row] - first_digit];
-  int digits_max;
+  unsigned long power[NUM_DIGITS[row] - firstDigit];
   int digit;
 
   /* pow() calculates with floats and gives wrong integers, i.e. 3^3 = 26. */
   power[0] = 1;
-  for (int i = 1; i < NUM_DIGITS[row] - first_digit; i++) {
-    power[i] = power[i - 1] * numeral_base;
+  for (int i = 1; i < NUM_DIGITS[row] - firstDigit; i++) {
+    power[i] = power[i - 1] * numeralBase;
   }
 
-  for (int i = NUM_DIGITS[row] - first_digit - 1; i >= 0; i--) {
+  for (int i = NUM_DIGITS[row] - firstDigit - 1; i >= 0; i--) {
     if (maximum > power[i]) {
       digit = 0;
       while (number >= power[i]) {
         digit++;
         number -= power[i];
       }
-      if (digit < numeral_base) {
-        lc.setChar(row, first_digit + i, digit, false);
+      if (digit < numeralBase) {
+        lc.setChar(row, firstDigit + i, digit, false);
       }
       else {
-        error(row, first_digit + i);
+        error(row, firstDigit + i);
         break;
       }
     }
@@ -433,10 +449,109 @@ void print_digits(int number, int row, int first_digit, int maximum, int numeral
 
 }
 
-void set_brightness() {
+void giveStopClock(int stopTime, bool update) {
+
+  if (update) {
+
+    if (lastSecond != second(stopTime)) {
+      lastSecond = second(stopTime);
+      stopCenti = 0;
+      stopDeci = 0;
+      isChanged = true;
+    }
+
+    while (millis() - lastEventMillis >= 10) {
+      lastEventMillis += 10;
+      if (stopCenti < 99) {
+        stopCenti++;
+      }
+    }
+
+    if (stopDeci != stopCenti / 10) {
+      stopDeci = stopCenti / 10;
+      isChanged = true;
+    }
+
+  }
+
+  if (base >= STOP_BASE_CENTI) {
+    printClock(stopCenti, second(stopTime), minute(stopTime));
+  }
+  else if (isChanged || !stopClockRunning) {
+    isChanged = false;
+    printClock(stopDeci, second(stopTime), minute(stopTime));
+  }
+
+}
+
+void changeBright(int upDown) {
+
+  /* 0: left button = down
+   * 2: right button = up
+   */
+
+  if (upDown == 0 && brightness > 0) {
+    brightness--;
+  }
+
+  if (upDown == 2 && brightness < 15) {
+    brightness++;
+  }
+
+}
+
+void setBright() {
 
   for (int i = 0; i < NUM_DRIVERS; i++) {
     lc.setIntensity(i, brightness);
+  }
+
+}
+
+void changeBase(int upDown) {
+
+  /* 0: left button = down
+   * 2: right button = up
+   */
+
+  if (upDown == 0 && base > 2) {
+    base--;
+  }
+
+  if (upDown == 2 && base < 16) {
+    base++;
+  }
+
+}
+
+void changeSetTime(int upDown) {
+
+  /* 0: left button = down
+   * 2: right button = up
+   */
+
+  if (upDown == 0) {
+    if (currentRow == 2 && setHour > 0) {
+      setHour--;
+    }
+    if (currentRow == 1 && setMinute > 0) {
+      setMinute--;
+    }
+    if (currentRow == 0 && setSecond > 0) {
+      setSecond--;
+    }
+  }
+
+  if (upDown == 2) {
+    if (currentRow == 2 && setHour < 24) {
+      setHour++;
+    }
+    if (currentRow == 1 && setMinute < 60) {
+      setMinute++;
+    }
+    if (currentRow == 0 && setSecond < 60) {
+      setSecond++;
+    }
   }
 
 }
@@ -451,7 +566,7 @@ void error(int row, int digit) {
 
 }
 
-void clear_displays() {
+void clearDisplays() {
 
   for (int i = 0; i < NUM_DRIVERS; i++) {
    lc.clearDisplay(i);
@@ -467,19 +582,19 @@ void readRTC() {
 
 void writeRTC() {
 
-  byte b_second;
-  byte b_minute;
-  byte b_hour;
+  byte bSecond;
+  byte bMinute;
+  byte bHour;
 
-  b_second = (setSecond/10*16) + (setSecond%10) & 0x7f;
-  b_minute = (setMinute/10*16) + (setMinute%10);
-  b_hour = (setHour/10*16) + (setHour%10) & 0x3f;
+  bSecond = (setSecond/10*16) + (setSecond%10) & 0x7f;
+  bMinute = (setMinute/10*16) + (setMinute%10);
+  bHour = (setHour/10*16) + (setHour%10) & 0x3f;
 
   Wire.beginTransmission(DS1307_I2C);
   Wire.write((byte)0x00);
-  Wire.write(b_second);
-  Wire.write(b_minute);
-  Wire.write(b_hour);
+  Wire.write(bSecond);
+  Wire.write(bMinute);
+  Wire.write(bHour);
   Wire.endTransmission();
 
 }
